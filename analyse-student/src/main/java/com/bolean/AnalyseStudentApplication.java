da32cc43f3ed389c0bf6026f1dc8f08e5d7699ec
wodeln <Lina521521>
2017-12-06 14:26:55 +0800

    log4j2 日志配置

--- analyse-student/src/main/java/com/bolean/AnalyseStudentApplication.java
+++ analyse-student/src/main/java/com/bolean/AnalyseStudentApplication.java
@@ -1,5 +1,9 @@
 package com.bolean;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +13,20 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan
 @MapperScan("com.bolean.dao")
 public class AnalyseStudentApplication {
-
+    private static final Logger log = LogManager.getLogger(AnalyseStudentApplication.class);
+    private static final Log log1 = LogFactory.getLog(AnalyseStudentApplication.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(AnalyseStudentApplication.class, args);
+
+        log.trace("这是 org.apache.logging.log4j LogManager trace log");
+        log.warn("这是 org.apache.logging.log4j LogManager warn log");
+        log.debug("这是 org.apache.logging.log4j LogManager debug log");
+        log.error("折是 org.apache.logging.log4j LogManager error log");
+        System.out.println("-----------------------------------------------------------华丽丽的分割线----------------------------------------------------------------");
+        log1.trace("这是 org.apache.commons.logging LogFactory trace log");
+        log1.warn("这是 org.apache.commons.logging LogFactory warn log");
+        log1.debug("这是 org.apache.commons.logging LogFactory debug log");
+        log1.error("这是 org.apache.commons.logging LogFactory error log");
 	}
 }
