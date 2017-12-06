d97859c1fc230a2ce30ef1190a119c0e62d27a49
wodeln <Lina521521>
2017-12-06 12:42:54 +0800

    模块化

---
+++ analyse-student/src/main/java/com/bolean/AnalyseStudentApplication.java
@@ -0,0 +1,12 @@
+package com.bolean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyseStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyseStudentApplication.class, args);
+	}
+}
