bb1de8fdd009a542c52f6d3261f0b2481a941d7e
wodeln <Lina521521>
2018-01-08 09:27:27 +0800

    优化引用包

--- analyse-student/src/main/java/com/bolean/conf/BeetlConf.java
+++ analyse-student/src/main/java/com/bolean/conf/BeetlConf.java
@@ -1,17 +1,12 @@
 package com.bolean.conf;
 
-import org.beetl.core.ResourceLoader;
 import org.beetl.core.resource.ClasspathResourceLoader;
-import org.beetl.core.resource.WebAppResourceLoader;
 import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
 import org.beetl.ext.spring.BeetlSpringViewResolver;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternUtils;
 
 import java.util.HashMap;
 import java.util.Map;
