5d37062695f1cfd537ff01d57c83ec85c3cf1e48
wodeln <Lina521521>
2018-02-01 13:39:53 +0800

    添加上传文件方法

--- analyse-student/src/main/java/com/bolean/controller/BaseController.java
+++ analyse-student/src/main/java/com/bolean/controller/BaseController.java
@@ -9,7 +9,14 @@ import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import utils.DateHelper;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -68,5 +75,46 @@ public class BaseController {
         }
 
         return grades;
+    }
+
+    private String executeUpload(String uploadDir,MultipartFile file) throws Exception
+    {
+        //文件后缀名
+        String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        //上传文件名
+        String filename = DateHelper.getCurDateTimeMI() + suffix;
+        //服务器端保存的文件对象
+        File serverFile = new File(uploadDir + filename);
+        //将上传的文件写入到服务器端文件内
+        file.transferTo(serverFile);
+
+        return filename;
+    }
+
+
+    protected Map<String, Object> upload(MultipartFile file)
+    {
+        Map<String,Object> map = new HashMap<>();
+        map.put("status","1");
+        map.put("oderName",file.getOriginalFilename());
+        try {
+            //上传目录地址
+            String uploadDir = ClassUtils.getDefaultClassLoader().getResource("").getPath() +"upload/"+DateHelper.getCurDate()+"/";
+            //如果目录不存在，自动创建文件夹
+            File dir = new File(uploadDir);
+            if(!dir.exists())
+            {
+                dir.mkdirs();
+            }
+            //调用上传方法
+            map.put("fileInfo",dir+"\\"+executeUpload(uploadDir+"",file));
+        }catch (Exception e)
+        {
+            //打印错误堆栈信息
+            map.put("status","0");
+            e.printStackTrace();
+        }
+
+        return map;
     }
 }
